32cfc24a1df46b5a1fc6655ae2dc5de1d181dade
madhan <madhanar@gmail.com>
2025-12-19 08:11:23 +0000

    Revert "tag owern change"
    This reverts commit 3855c149e1b2d9742badc7b6ff335eda2ca335cb.

--- a5.local-variables.tf
+++ a5.local-variables.tf
@@ -2,7 +2,7 @@ locals {
   resource_name_prefix = "${var.business_unit}-${var.environment}"
   #sap-dev
   project_tags = { #this is the name
-    Owner       = "SAP1"
+    Owner       = "SAP"
     environment = "dev"
     Project     = "kyn"
   }
