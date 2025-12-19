3855c149e1b2d9742badc7b6ff335eda2ca335cb
madhan <madhanar@gmail.com>
2025-12-19 08:07:33 +0000

    tag owern change

--- a5.local-variables.tf
+++ a5.local-variables.tf
@@ -2,7 +2,7 @@ locals {
   resource_name_prefix = "${var.business_unit}-${var.environment}"
   #sap-dev
   project_tags = { #this is the name
-    Owner       = "SAP"
+    Owner       = "SAP1"
     environment = "dev"
     Project     = "kyn"
   }
