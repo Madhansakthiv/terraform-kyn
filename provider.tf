ee9d7d240c74f816b10599faac5235fe4915d874
madhan <madhanar@gmail.com>
2025-12-19 07:25:14 +0000

    changing the instance size with development

--- provider.tf
+++ provider.tf
@@ -17,7 +17,7 @@ terraform {
 
 provider "azurerm" {
   # resource_provider_registrations = "none" # This is only required when the User, Service Principal, or Identity running Terraform lacks the permissions to register Azure Resource Providers.
-  features {}
+  features {} 
   subscription_id = var.subscription_id
   client_id = var.client_id
   client_secret = var.client_secret
